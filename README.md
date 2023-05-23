1621b1dea524f043d6cd95a81391a8afd6b78de4
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-05-23 23:01:56 +0430

    add vazirmant() and export for using

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ npm install persian-typography
 Usage
 After installing Persian Typography, you can import.
 ```js
+// first import font face you want
 import { vazirmatn } from "persian-typography"
 
+// then call vazirmatn() to apply font to document
+vazirmatn()
 ```
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 function vazirmatn(){
-	console.log("vazir matn")
+	// Select head element
+	const head = document.getElementsByTagName("head")[0]
+	// create link element to styles file
+	const vazirmatnFontFace = "<link rel='stylesheet' href='./src/font-face/vazirmatn.css'>"
+	// append new Link element to head of html file
+	head.insertAdjacentHTML("beforeend",vazirmatnFontFace )
+
 }
+export {vazirmatn}
